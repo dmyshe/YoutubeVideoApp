9d0679bd351b183d7d2be12d44629b3017f1f569
dmyshe <94231197+dmyshe@users.noreply.github.com>
2022-08-28 23:57:23 +0300

    Refactoring code

--- YoutubeApp/Services/YouTubeServiceAdapter.swift
+++ YoutubeApp/Services/YouTubeServiceAdapter.swift
@@ -10,12 +10,12 @@ import GoogleAPIClientForREST
 
 class YouTubeServiceAdapter {
     private let service = GTLRYouTubeService()
-        
+    
     init(apiKey: String) {
         service.apiKey = apiKey
     }
     
-    public func getChannelInfo(channelID: String ,queryPart: String = "snippet,brandingSettings,statistics") async -> Channel  {
+    public func getChannelInfo(channelID: String, queryPart: String = "snippet,brandingSettings,statistics") async -> Channel  {
         await withCheckedContinuation { continuation in
             self.getChannelInfo(channelID: channelID) { channel in
                 continuation.resume(returning: channel)
@@ -23,13 +23,40 @@ class YouTubeServiceAdapter {
         }
     }
     
+    // FIXME: - Doesn't work
+    public func getRandomChannel() async -> [Channel] {
+        return [Channel]()
+    }
+    
+    // FIXME: - Doesn't work
+    func getFullChannelInfo(channelID: String) async -> Channel {
+        var channel = await  getChannelInfo(channelID: channelID)
+        var playlist = await getVideoPlaylists(withChannelID: channelID).first
+        let videosss = await getPlaylistVideos(withPlaylistID: playlist!.id)
+        
+        playlist!.videos = videosss
+        channel.videoPlaylists!.append(playlist!)
+        
+        return channel
+    }
+    
+    
     public func getFullPlaylistVideos(playlistID id: String, queryPart: String = "snippet") async -> [Video] {
-        let videos = await getPlaylistVideos(playlistID: id)
+        let videos = await getPlaylistVideos(withPlaylistID: id)
         let playlist = await getPlaylistInfo(playlistID: id)
         
         videos.forEach { $0.playlist = playlist }
         
         return videos
+    }
+    
+    public func getVideoPlaylists(withChannelID id: String ,
+                                  queryPart: String = "snippet") async -> [Playlist] {
+        await withCheckedContinuation { continuation in
+            self.getVideoPlaylists(with: id) { playlists in
+                continuation.resume(returning: playlists)
+            }
+        }
     }
     
     public func getVideoViewCount(byID id: String, queryPart: String = "statistics") async -> Int {
@@ -41,7 +68,7 @@ class YouTubeServiceAdapter {
     }
     
     
-    public func getPlaylistVideos( playlistID id: String, queryPart: String = "snippet") async -> [Video] {
+    public func getPlaylistVideos( withPlaylistID id: String, queryPart: String = "snippet") async -> [Video] {
         await withCheckedContinuation { continuation in
             self.getPlaylistVideos(playlistID: id) { videos in
                 continuation.resume(returning: videos)
@@ -60,8 +87,37 @@ class YouTubeServiceAdapter {
 
 
 extension YouTubeServiceAdapter {
+    // FIXME: - Doesn't work
+    func getRandomChannels(queryPart: String = "snippet") {
+        let query =  GTLRYouTubeQuery_SearchList.query(withPart: "snippet")
+        query.q = "music"
+        query.type = "video"
+        query.maxResults = 15
+        
+        
+        //        service.shouldFetchNextPages = true
+        
+        service.executeQuery(query) { ticket, response, error in
+            
+            print(ticket)
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            guard let channelInfo = response as? GTLRYouTube_SearchListResponse,
+                  let channelID = channelInfo.items?.first?.snippet?.channelId
+            else { return }
+            
+            dump(channelID)
+        }
+        
+    }
     
-    private func getChannelInfo(channelID: String ,queryPart: String = "snippet,brandingSettings,statistics",completionHandler: @escaping (Channel) -> Void ) {
+    private func getChannelInfo(channelID: String,
+                                queryPart: String = "snippet,brandingSettings, statistics",
+                                completionHandler: @escaping (Channel) -> Void ) {
+        
         let channelQuery = GTLRYouTubeQuery_ChannelsList.query(withPart: queryPart)
         channelQuery.identifier = channelID
         
@@ -83,12 +139,15 @@ extension YouTubeServiceAdapter {
                                       backGroundBannerURL: URL(string: channelBGBannerURL)!))
         }
     }
-
-    private func getPlaylistVideos( playlistID id: String, queryPart: String = "snippet", completionHandler: @escaping ([Video]) -> Void) {
+    
+     func getPlaylistVideos( playlistID id: String,
+                                    queryPart: String = "snippet",
+                                    completionHandler: @escaping ([Video]) -> Void) {
+        
         let playlistItemsListQuery = GTLRYouTubeQuery_PlaylistItemsList.query(withPart: queryPart)
         playlistItemsListQuery.playlistId = id
-        playlistItemsListQuery.maxResults = 10
-
+        //        playlistItemsListQuery.maxResults = 10
+        
         service.executeQuery(playlistItemsListQuery) { _, response, error in
             if let error = error {
                 print(error.localizedDescription)
@@ -102,7 +161,7 @@ extension YouTubeServiceAdapter {
             }
             
             var playlistVideos = [Video]()
-           
+            
             
             for item in items {
                 guard let videoInfo = item.snippet,
@@ -122,10 +181,13 @@ extension YouTubeServiceAdapter {
     }
     
     
-    private func getVideoViewCount(byID id: String, queryPart: String = "statistics", completionHandler: @escaping (Int) -> Void) {
+    private func getVideoViewCount(byID id: String,
+                                   queryPart: String = "statistics",
+                                   completionHandler: @escaping (Int) -> Void) {
+        
         let videoQuery = GTLRYouTubeQuery_VideosList.query(withPart: queryPart)
         videoQuery.identifier = id
-
+        
         service.executeQuery(videoQuery) { _, response, error in
             if let error = error {
                 print(error.localizedDescription)
@@ -137,8 +199,10 @@ extension YouTubeServiceAdapter {
             completionHandler(viewCount)
         }
     }
-
-    private func getVideoPlaylistTitle(playlistID id: String, queryPart: String = "snippet",completionHandler: @escaping (Playlist) -> Void) {
+    
+    private func getVideoPlaylistTitle(playlistID id: String,
+                                       queryPart: String = "snippet",
+                                       completionHandler: @escaping (Playlist) -> Void) {
         
         let playlistListQuery = GTLRYouTubeQuery_PlaylistsList.query(withPart: queryPart)
         playlistListQuery.identifier = id
@@ -157,7 +221,38 @@ extension YouTubeServiceAdapter {
                 print("response items is empty")
                 return
             }
-           completionHandler(Playlist(name: playlistName, id: playListID))
+            completionHandler(Playlist(name: playlistName, id: playListID))
+        }
+    }
+    
+    private func getVideoPlaylists(with id: String ,
+                                   queryPart: String = "snippet", completionHandler: @escaping ([Playlist]) -> Void) {
+        let playlistListQuery = GTLRYouTubeQuery_PlaylistsList.query(withPart: queryPart)
+        playlistListQuery.channelId = id
+        
+        service.executeQuery(playlistListQuery)  { _, response, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            guard let response = response as? GTLRYouTube_PlaylistListResponse else { return }
+            
+            guard let items = response.items  else {
+                print("response items is empty")
+                return
+            }
+            
+            var playlists = [Playlist]()
+            
+            items.forEach { item in
+                if let title = item.snippet?.title, let id = item.identifier {
+                    let playlist = Playlist(name: title, id: id)
+                    playlists.append(playlist)
+                }
+            }
+            
+            
+            completionHandler(playlists)
         }
     }
 }
